1acf2a49440d9658ab6519e81b393f9a43739451
Ilya <iyudkovich@kyruus.com>
2021-02-22 18:51:30 -0500

    refactor

--- models/zillow.py
+++ models/zillow.py
@@ -1,11 +1,16 @@
-from typing import Optional, Dict
+from typing import Optional, Dict, Any
 
 from pydantic import BaseModel
 
+from constants import ZILLOW_URL, ZILLOW_QUERY_PARAMS, ZILLOW_QUERY_HEADERS
 
-class ZillowResponse(BaseModel):
-    full_address: Optional[str]
-    street_name: Optional[str]
+
+class ZillowData(BaseModel):
+    name: Optional[str]
+    type: Optional[str]
+    number_of_rooms: Optional[str]
+    address: Optional[Any] #remodel this to be of class Address
+    street_name: Optional[Any]
     state: Optional[str]
     zip_code: Optional[str]
     property_url: Optional[str]
@@ -16,7 +21,19 @@ class ZillowResponse(BaseModel):
         return self.__fields__
 
 
+class Address(BaseModel):
+    """
+    TODO: capture all the fields based on the following data
+    'address': {'@type': 'PostalAddress', '@context': 'http://schema.org', 'streetAddress': '3903 Perkins Rd', 'addressLocality': 'Thompsons Station', 'addressRegion': 'TN', 'postalCode': '37179'}
+    """
+    pass
+
+
 class ZillowRequest(BaseModel):
-    url: Optional[str]
-    params: Optional[Dict]
-    headers: Optional[Dict]
+    url: Optional[str] = ZILLOW_URL
+    params: Optional[Dict] = ZILLOW_QUERY_PARAMS
+    headers: Optional[Dict] = ZILLOW_QUERY_HEADERS
+
+    def add_params(self, params:dict):
+        self.params.update(params)
+
--- zillow.py
+++ zillow.py
@@ -8,24 +8,61 @@ from typing import List
 
 from bs4 import BeautifulSoup
 import requests
-from models.zillow import ZillowResponse, ZillowRequest
-
-from constants import ZILLOW_URL, ZILLOW_QUERY_HEADERS, ZILLOW_QUERY_PARAMS
+from models.zillow import ZillowData, ZillowRequest
 
 
 class ZillowScraper():
 
     def __init__(self):
-        self.results: List[ZillowResponse] = []
+        self.results: List[ZillowData] = []
         self.output_file = 'zillow_listings.csv'
 
     def fetch(self, zillow_request: ZillowRequest):
-        response = requests.get(zillow_request.url,
+        response = requests.get(url=zillow_request.url,
                                 headers=zillow_request.headers,
                                 params=zillow_request.params)
 
-        print(response)
         return response
+
+    def get_zillow_urls_per_property(self, response) -> List[str]:
+        list_of_urls = []
+        content = BeautifulSoup(response, features='lxml')
+
+        property_cards = content.find(
+            'ul', {'class': 'photo-cards photo-cards_wow photo-cards_short'}
+        )
+
+        for child_property in property_cards:
+            property_data = child_property.find(
+                'script', {'type': 'application/ld+json'}
+            )
+
+            if property_data:
+                property_data_json = json.loads(property_data.contents[0])
+
+                list_of_urls.append(
+                    property_data_json['url']
+                )
+
+        return list_of_urls
+
+    def get_property_details(self, url: str) -> ZillowData:
+
+        zillow_request = ZillowRequest(url=url)
+        response = self.fetch(zillow_request)
+        content = BeautifulSoup(response.text, features='lxml')
+        json_text = json.loads(content.find_all(
+            "script", {"type": "application/ld+json"})[1].string)
+        print(json_text)
+        zillow_data_object = ZillowData(
+            type=json_text['@type'],
+            name=json_text['name'],
+            number_of_rooms=json_text['numberOfRooms'],
+            address=json_text['address'],
+            property_url=json_text['url']
+        )
+
+        return zillow_data_object
 
     def parse(self, response):
         content = BeautifulSoup(response, features='lxml')
@@ -37,17 +74,26 @@ class ZillowScraper():
             if property_data:
                 property_data_json = json.loads(property_data.contents[0])
 
-                zillow_response = ZillowResponse(
-                    full_address=property_data_json['name'],
-                    street_name=property_data_json['address']['streetAddress'],
-                    state=property_data_json['address']['addressRegion'],
-                    zip_code=property_data_json['address']['postalCode'],
-                    property_url=property_data_json['url'],
-                    square_footage=property_data_json['floorSize']['value'],
-                    sales_prices=child_property.find('div', {'class': 'list-card-price'}).text
-                )
+                url_for_property = property_data_json['url']
 
-                self.results.append(zillow_response)
+                follow_up_request = ZillowRequest(url=url_for_property)
+                response = self.fetch(follow_up_request)
+
+                content = BeautifulSoup(response.text, features='lxml')
+
+                break
+
+                # zillow_response = ZillowData(
+                #     full_address=property_data_json['name'],
+                #     street_name=property_data_json['address']['streetAddress'],
+                #     state=property_data_json['address']['addressRegion'],
+                #     zip_code=property_data_json['address']['postalCode'],
+                #     property_url=property_data_json['url'],
+                #     square_footage=property_data_json['floorSize']['value'],
+                #     sales_prices=child_property.find('div', {'class': 'list-card-price'}).text
+                # )
+                #
+                # self.results.append(zillow_response)
 
     def write_to_csv(self):
         with open(self.output_file, 'w') as csv_file:
@@ -60,14 +106,11 @@ class ZillowScraper():
 
     def run(self):
 
-        request = ZillowRequest(
-            url=ZILLOW_URL,
-            params=ZILLOW_QUERY_PARAMS,
-            headers=ZILLOW_QUERY_HEADERS
-        )
+        request = ZillowRequest()
 
         response = self.fetch(request)
-        self.parse(response.text)
+        urls = self.get_zillow_urls_per_property(response.text)
+        self.results.append(self.get_property_details(urls[0]))
         self.write_to_csv()
 
 
